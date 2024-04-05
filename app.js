f2977b72f646d437aed957b0cd95886fe4fc675e
Harshit Pandey <helloharshit68@gmail.com>
2024-04-04 21:45:20 -0400

    Add completed lab

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
+import Lab5 from './lab_5.js';
+import Hello from './hello.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+Hello(app);
+Lab5(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
