c99c4c72e037c993a5a6653748db9f8c64b3d60d
Harshit Pandey <helloharshit68@gmail.com>
2024-04-17 16:20:31 -0400

    Assignment 6

--- app.js
+++ app.js
@@ -3,17 +3,47 @@ import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
-import Lab5 from './lab_5.js';
-import Hello from './hello.js';
+import Lab5 from "./lab_5.js";
+import Hello from "./hello.js";
+import mongoose from "mongoose";
+import UserRoutes from "./Kanbas/Users/routes.js";
+import session from "express-session";
+import "dotenv/config";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+
 app.use(express.json());
+
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.HTTP_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
+
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 
 Hello(app);
 Lab5(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 
 app.listen(process.env.PORT || 4000);
